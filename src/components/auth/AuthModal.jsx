1aadc174a3c62aff6ea448df1c4b57368874fae2
DannyBorbon <dannyborbon03@gmail.com>
2026-08-24 19:42:58 -0700

    feat: add Google login button to auth modal

--- src/components/auth/AuthModal.jsx
+++ src/components/auth/AuthModal.jsx
@@ -1,4 +1,19 @@
 import "./AuthModal.css";
+import googleIconSvg from "../../assets/images/auth/google-icon.svg";
+
+const login = {
+  subtitle: "Login",
+  title: "Welcome Back",
+  description:
+    "Enter your credentials to continue your neural reinforcement journey.",
+};
+
+const register = {
+  subtitle: "Register",
+  title: "Join the evolution",
+  description:
+    "Enter your credentials to continue your neural reinforcement journey.",
+};
 
 function AuthModal({ children }) {
   return (
@@ -12,6 +27,15 @@ function AuthModal({ children }) {
             journey.
           </div>
           <div className="auth__form-area">{children}</div>
+          <div className="auth__separator">
+            <span className="auth__separator-line"></span>
+            <span className="auth__separator-text">OR</span>
+            <span className="auth__separator-line"></span>
+          </div>
+          <button className="signin__google-btn">
+            <img src={googleIconSvg} alt="" />
+            Continue with Google
+          </button>
           <div className="auth__footer">
             <button>Don't have an account? Register</button>
           </div>
--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,10 +1,37 @@
-import "./AuthModal.css";
+import "./LoginForm.css";
 
 function LoginForm() {
   return (
-    <div className="loginform">
-      <div className="loginform__container">asd</div>
-    </div>
+    <form className="loginform" action="">
+      <div className="loginform__field">
+        <label className="loginform__label" type="email">
+          Email
+        </label>
+        <input
+          className="loginform__input"
+          id="email"
+          type="email"
+          placeholder="name@email.com"
+        />
+        <div className="loginform__field-label">
+          <label className="loginform__label" type="password">
+            Password
+          </label>
+          <a className="loginform__forgot" href="">
+            Forgot?
+          </a>
+        </div>
+        <input
+          className="loginform__input"
+          id="email"
+          type="email"
+          placeholder="••••••••"
+        />
+        <button className="loginform__submit" type="submit">
+          LOG IN
+        </button>
+      </div>
+    </form>
   );
 }
 
